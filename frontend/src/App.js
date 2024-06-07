8a74a794e587829087fd96882ed310d0cbd409c3
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2024-06-07 18:00:36 +0530

    Media Query for tab and laptop view done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import Layout from '../src/pages/Layout';
-import Footer from '../src/components/Footer'
+import Footer from '../src/components/Footer';
+import Resource from '../src/components/Resource'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      
-      <Routes>
-        <Route path="/" element={Layout}></Route>
-      </Routes>
-      <Layout/>
-      <Footer/>
-      </BrowserRouter>
+       <BrowserRouter>
+           <Routes>
+                 <Route path="/Home" element={Layout}></Route>
+                 <Route path="/Resource" element={Resource}></Route>
+                 <Route path="/Pricing" element={Resource}></Route>
+                 <Route path="/Customer" element={Resource}></Route>
+          
+           </Routes>
+           <Layout/>
+           <Footer/>
+       </BrowserRouter>
         
     </div>
   );
---
+++ frontend/src/components/Resource.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const Resource = () => {
+  return (
+    <div></div>
+  )
+}
+export default Resource;

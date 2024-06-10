a6372075609b32ebe4e4da52cbbe4ff126562fba
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2024-06-10 11:48:08 +0530

    Tailwind css setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 import Layout from '../src/pages/Layout';
 import Footer from '../src/components/Footer';
 import Resource from '../src/components/Resource'
+import Pricing from '../src/components/Pricing'
+import  Customer from '../src/components/Customer';
+import Navbar from '../src/components/Navbar';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      
        <BrowserRouter>
+      
+       <Layout/>
            <Routes>
-                 <Route path="/Home" element={Layout}></Route>
-                 <Route path="/Resource" element={Resource}></Route>
-                 <Route path="/Pricing" element={Resource}></Route>
-                 <Route path="/Customer" element={Resource}></Route>
+                 <Route path="/Home" element={<Layout/>}></Route>
+                 <Route path="/Resource" element={<Resource/>}></Route>
+                 <Route path="/Pricing" element={<Pricing/>}></Route>
+                 <Route path="/Customer" element={<Customer/>}></Route>
           
            </Routes>
-           <Layout/>
+           
            <Footer/>
        </BrowserRouter>
         

9a8ae5008f5b4d25ef069f8e150642d9daf08d0b
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2024-06-10 17:43:58 +0530

    Day 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Layout from '../src/pages/Layout';
-import Footer from '../src/components/Footer';
 import Resource from '../src/components/Resource'
-import Pricing from '../src/components/Pricing'
-import  Customer from '../src/components/Customer';
+import Instructor from '../src/components/Instructor'
+import Workshops from '../src/components/Workshops';
 import Navbar from '../src/components/Navbar';
+import MyBooking from '../src/components/MyBooking'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
   return (
@@ -16,9 +16,9 @@ function App() {
            <Routes>
                  <Route path="/" element={<Layout/>}></Route>
                  <Route path="/Resource" element={<Resource/>}></Route>
-                 <Route path="/Pricing" element={<Pricing/>}></Route>
-                 <Route path="/Customer" element={<Customer/>}></Route>
-          
+                 <Route path="/Ourinstructor" element={<Instructor/>}></Route>
+                 <Route path="/Workshops" element={<Workshops/>}></Route>
+                 <Route path="/MyBooking" element={<MyBooking/>}></Route>
            </Routes>
            
            

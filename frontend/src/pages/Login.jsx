735cf9400dbebe4ff8fba0648a1dc3c420117965
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2024-06-20 10:04:10 +0530

    SERVER FOR AUTH FIXED

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import loginImg from "../assets/images/login.webp"
+import Template from "../components/Template"
+
+function Login() {
+  return (
+    <div style={{background:"linear-gradient(180deg, rgb(0, 0, 0) 10%, rgb(55, 94, 99) 50%)"}} >
+    <Template
+      title="Welcome Back"
+      description1="Build skills for today, tomorrow, and beyond."
+      description2="Education to future-proof your career."
+      image={loginImg}
+      formType="login"
+    />
+    </div>
+  )
+}
+
+export default Login
